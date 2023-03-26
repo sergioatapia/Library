3b1afae3f8d323be52eff08b4c7c66de7ae20ad2
sergioatapia <126285852+sergioatapia@users.noreply.github.com>
2023-03-26 15:27:03 -0500

    Added setters and getters to Book class

--- Library.java
+++ Library.java
@@ -15,6 +15,38 @@ class Book {
       this.available = available;
   }
   
+  // Getters and setters for book information
+  public int getId() {
+      return id;
+  }
+
+  public void setId(int id) {
+      this.id = id;
+  }
+
+  public String getTitle() {
+        return title;
+  }
+
+  public void setTitle(String title) {
+      this.title = title;
+  }
+
+  public String getAuthor() {
+        return author;
+  }
+
+  public void setAuthor(String author) {
+      this.author = author;
+  }
+
+  public boolean isAvailable() {
+      return available;
+  }
+
+  public void setAvailable(boolean available) {
+      this.available = available;
+  }
 }
 
 // Librarian class represents a librarian with corresponding authorizations
