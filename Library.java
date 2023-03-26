d2cc12724e4267e8c92bcd39f500e3c6324c4ebd
sergioatapia <126285852+sergioatapia@users.noreply.github.com>
2023-03-26 15:32:55 -0500

    Added setters and getters for User class

--- Library.java
+++ Library.java
@@ -102,4 +102,36 @@ class User {
       borrowedBooks = new ArrayList<>();
   }
   
+  // Getters and setters for user information
+  public int getId() {
+      return id;
+  }
+
+  public void setId(int id) {
+      this.id = id;
+  }
+
+  public String getName() {
+      return name;
+  }
+
+  public void setName(String name) {
+      this.name = name;
+  }
+
+  public String getEmail() {
+      return email;
+  }
+
+  public void setEmail(String email) {
+      this.email = email;
+  }
+
+  public ArrayList<Book> getBorrowedBooks() {
+      return borrowedBooks;
+  }
+
+  public void setBorrowedBooks(ArrayList<Book> borrowedBooks) {
+      this.borrowedBooks = borrowedBooks;
+  }
 }
