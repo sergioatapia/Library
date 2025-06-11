6c87343c5730bb60f3d5153510f150193205f3e9
Your Name <sergioatapia315@gmail.com>
2025-06-11 09:53:50 -0500

    Add core Library logic with book add/remove methods

--- Library.java
+++ Library.java
@@ -1,174 +1,22 @@
-/* Library Management System
- * Author: Sergio Alvarez Tapia
- * Created: Mar, 2023
- */
-
 import java.util.ArrayList;
-import java.util.Scanner;
 
-// Book class represens a book in the library and its information
-class Book {
-    private int id;
-    private String title;
-    private String author;
-    private boolean available;
+public class Library {
+    private ArrayList<String> books;
 
-    public Book(int id, String title, String author, boolean available) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.available = available;
-    }
-  
-    // Getters and setters for book information
-    public int getId() {
-        return id;
+    public Library() {
+        books = new ArrayList<>();
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void addBook(String title) {
+        books.add(title);
     }
 
-    public String getTitle() {
-          return title;
+    public boolean removeBook(String title) {
+        return books.remove(title);
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-          return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public boolean isAvailable() {
-        return available;
-    }
-
-    public void setAvailable(boolean available) {
-        this.available = available;
-    }
-    
-    // Override toString() method to display book information
-    @Override
-    public String toString() {
-        return "Book {" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", available=" + available +
-                '}';
+    public ArrayList<String> getBooks() {
+        return books;
     }
 }
 
-// Librarian class represents a librarian with corresponding authorizations
-class Librarian {
-    private int id;
-    private String name;
-    private String email;
-
-    public Librarian(int id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-  
-    // Getters and setters for librarian information
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
-    // Override toString() method to display librarian information
-    @Override
-    public String toString() {
-        return "Librarian {" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
-}
-
-
-// User class represents a normal user that is not a librarian
-class User {
-    private int id;
-    private String name;
-    private String email;
-    private ArrayList<Book> borrowedBooks;
-
-    public User(int id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        borrowedBooks = new ArrayList<>();
-    }
-  
-    // Getters and setters for user information
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public ArrayList<Book> getBorrowedBooks() {
-        return borrowedBooks;
-    }
-
-    public void setBorrowedBooks(ArrayList<Book> borrowedBooks) {
-        this.borrowedBooks = borrowedBooks;
-    }
-    
-    // Override toString() method to display user information
-    @Override
-    public String toString() {
-        return "User {" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", borrowedBooks=" + borrowedBooks +
-                '}';
-    } 
-}
